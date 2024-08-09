c8b6b5ac9f4259ce746a43e6d4c61daa28a8691d
yofujitsu <yofujitsuuu@gmail.com>
2024-08-09 14:35:30 +0300

    init

---
+++ src/test/java/com/yofujitsu/cicd/CicdTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yofujitsu.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
